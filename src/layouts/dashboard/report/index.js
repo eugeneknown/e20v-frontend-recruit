6ed58ef29cc7b263ef52e0c63b9a803256511def
jin <eugeneknown@gmail.com>
2025-02-12 18:53:38 +0800

    Eugene > Frontend > updat

--- src/layouts/dashboard/report/index.js
+++ src/layouts/dashboard/report/index.js
@@ -32,7 +32,7 @@ import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
 import PieChart from "examples/Charts/PieChart";
 import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { axiosPrivate } from "api/axios";
 import { useNavigate, useLocation } from "react-router-dom";
 import moment from "moment";
@@ -49,6 +49,8 @@ import colors from "assets/theme/base/colors";
 import reportsBarChartData from "../data/reportsBarChartData";
 import { formatDateTime } from "global/function";
 import configs from "./configs";
+import SwipeableViews from "react-swipeable-views";
+import DataTable from "examples/Tables/DataTable";
 
 
 function Report() {
@@ -56,6 +58,7 @@ function Report() {
   const { e20 } = colors
 
   const { auth } = useAuth()
+  const [categorize, setCategorize] = useState('report')
   const [report, setReport] = useState()
   const [reportSeries, setReportSeries] = useState('platforms')
   const [reportDate, setReportDate] = useState({ start: moment().startOf('month'), end: moment().endOf('month') })
@@ -70,6 +73,13 @@ function Report() {
   // const [ monthEnd, setMonthEnd ] = useState(moment().endOf('year'))
 
   const [step, setStep] = useState(0)
+
+  const swipeRef = useRef()
+  const handleSwipeHeight = () => {
+    setTimeout(() => {
+      if (swipeRef) swipeRef.current.updateHeight()
+    }, 500);
+  }
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -168,7 +178,7 @@ function Report() {
         {
           target: 'created_at',
           operator: 'range',
-          value: [reportDate['start'], reportDate['end']],
+          value: categorize=='report'?[reportDate['start'], reportDate['end']]:reportDate,
         }
       ],
       [reportSeries]: {
@@ -176,60 +186,98 @@ function Report() {
         operator: reportBy,
         value: 'id',
       },
+      category: categorize,
     }).then((result) => {
       console.log('debug report data:', result)
       result = result.data['entity_career']
 
-      var dataSeries = []
-      var dataSets = []
-
-      for ( var i in result['series'] ) {
-        var data = result['series'][i]
-        dataSeries.push({
-          'dataKey': data['id'],
-          'label': data['title'],
-          'color': data['color'],
-        })
-      }
-
-      for ( var i in result['sets'] ) {
-        var tempData = {}
-
-        var data = result['sets'][i]
-        tempData['date'] = formatDateTime(i, 'MMMM DD YYYY')
-        tempData['total'] = data['total']
-        for ( var j in data['data'] ) {
-          tempData[data['data'][j]['id']] = data['data'][j].value
+      if ( categorize == 'report' ) {
+        var dataSeries = []
+        var dataSets = []
+  
+        for ( var i in result['series'] ) {
+          var data = result['series'][i]
+          dataSeries.push({
+            'dataKey': data['id'],
+            'label': data['title'],
+            'color': data['color'],
+          })
         }
-
-        dataSets.push(tempData)
-      }
-
-      if ( reportType == 'week' ) {
-        setReportProps({
-          topAxis: {
-            disableLine: true,
-            disableTicks: true,
-            valueFormatter: (value, context) => {
-              if (context.location == 'tick') {
-                return `${formatDateTime(value, 'ddd')}`
+  
+        for ( var i in result['sets'] ) {
+          var tempData = {}
+  
+          var data = result['sets'][i]
+          tempData['date'] = formatDateTime(i, 'MMMM DD YYYY')
+          tempData['total'] = data['total']
+          for ( var j in data['data'] ) {
+            tempData[data['data'][j]['id']] = data['data'][j].value
+          }
+  
+          dataSets.push(tempData)
+        }
+  
+        if ( reportType == 'week' ) {
+          setReportProps({
+            topAxis: {
+              disableLine: true,
+              disableTicks: true,
+              valueFormatter: (value, context) => {
+                if (context.location == 'tick') {
+                  return `${formatDateTime(value, 'ddd')}`
+                }
               }
             }
-          }
-        })
-      } else {
-        setReportProps({})
+          })
+        } else {
+          setReportProps({})
+        }
+  
+        console.log('debug weekly report datasets', dataSeries, dataSets);
+        setReport({ dataSeries, dataSets })
       }
 
-      console.log('debug weekly report datasets', dataSeries, dataSets);
-      setReport({ dataSeries, dataSets })
+      if ( categorize == 'comparison' ) {
+        var row = []
+        Object.keys(result).map((item, index) => {
+          row.push({
+            title: '',
+            result1: '',
+            result2: '',
+            percent: '',
+          })
+        })
+      }
+
+      handleSwipeHeight()
       
     }).catch((err) => {
       console.log('monthly recruit error data:', err)
     })
   },[reportSeries, reportBy, reportDate])
 
+  useEffect(() => {
+    if (categorize == 'report') {
+
+    } else 
+    if ( categorize == 'comparison' ) {
+      SetReportType(byDate['currMonth'].type)
+      setReportDate({
+        date1: { start: byDate['lastMonth'].start, end: byDate['lastMonth'].end },
+        date2: { start: byDate['currMonth'].start, end: byDate['currMonth'].end },
+      })
+      SetReportBy(byDate['currMonth'].by)
+    }
+  },[categorize])
+
   var tabs = ['Report', 'Comparison']
+
+  const columns = [
+    { Header: "report", accessor: "title", align: "left" },
+    { Header: "Total Result 1", accessor: "result1", align: "center" },
+    { Header: "Total Result 2", accessor: "result2", align: "center" },
+    { Header: "%", accessor: "percent", align: "center" },
+  ]
 
   const CustomLegend = ({ series }) => {
     return (
@@ -251,7 +299,7 @@ function Report() {
         <Grid2 container>
           <Grid2 size={{ xs: 4 }}>
             <AppBar position="static">
-                <Tabs value={step} onChange={(e, val) => {setStep(val)}}>
+                <Tabs value={step} onChange={(e, val) => {setStep(val); setCategorize(String(tabs[val]).toLowerCase())}}>
                   {tabs.map(item => (
                     <Tab
                       label={item}
@@ -305,35 +353,50 @@ function Report() {
                   </FormControl>
                 </MDBox>
               </MDBox>
-              <MDBox p="1rem">
-                {
-                  report &&
-                  <BarChart
-                    dataset={report['dataSets']}
-                    {...reportProps}
-                    {...options}
-                    series={
-                      Object.keys(report['dataSeries']).map((item, key) => {
-                        // console.log('debug series data', item, key, report);
-                        var series = {
-                          ...report['dataSeries'][item],
-                          valueFormatter: (value, context) => {
-                            // console.log('debug series value monthly formatter:', value, context);
-                            if (context.dataIndex >= 0) {
-                              var total = report['dataSets'][context.dataIndex].total
-                              var percentage = total != 0 ? Math.round((value/total)*100) : 0
-                              // return `Total ${value} -> ${percentage}%`
-                              return `${percentage}%`
+              <SwipeableViews
+                index={step}
+                animateHeight
+                ref={swipeRef}
+              >
+                <MDBox p='1rem'>
+                  {
+                    report &&
+                    <BarChart
+                      dataset={report['dataSets']}
+                      {...reportProps}
+                      {...options}
+                      series={
+                        Object.keys(report['dataSeries']).map((item, key) => {
+                          // console.log('debug series data', item, key, report);
+                          var series = {
+                            ...report['dataSeries'][item],
+                            valueFormatter: (value, context) => {
+                              // console.log('debug series value monthly formatter:', value, context);
+                              if (context.dataIndex >= 0) {
+                                var total = report['dataSets'][context.dataIndex].total
+                                var percentage = total != 0 ? Math.round((value/total)*100) : 0
+                                // return `Total ${value} -> ${percentage}%`
+                                return `${percentage}%`
+                              }
                             }
                           }
-                        }
-                        return series
-                      })
-                    }
-                  />
-                }
-                {report && <CustomLegend series={report['dataSeries']} />}
-              </MDBox>
+                          return series
+                        })
+                      }
+                    />
+                  }
+                  {report && <CustomLegend series={report['dataSeries']} />}
+                </MDBox>
+                <MDBox p='1rem'>
+                  {/* <DataTable
+                    table={{ columns, report }}
+                    isSorted={false}
+                    entriesPerPage={false}
+                    showTotalEntries={false}
+                    noEndBorder
+                  /> */}
+                </MDBox>
+              </SwipeableViews>
             </Grid2>
           </Grid2>
         </Card>
